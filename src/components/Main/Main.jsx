009c082f9d19c8eb14ca3213ce0ddd97f780927b
Elijah <xrealntrue16x@gmail.com>
2024-09-02 12:00:36 -0400

    WeatherCard Image complete

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,19 @@
+import "./Main.css";
+import WeatherCard from "../WeatherCard/WeatherCard";
+
+function Main() {
+  return (
+    <>
+      <main>
+        <WeatherCard />
+        <section className="cards">
+          <p className="card__text">
+            Today is 75&deg; F / You may want to wear:
+          </p>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default Main;
