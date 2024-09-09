bfc6b00d08e87bf246060ac7dd11dbe6fb67705c
Elijah <xrealntrue16x@gmail.com>
2024-09-09 16:02:10 -0400

    Footer Fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,12 +2,10 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <>
-      <div className="footer">
-        <p className="footer__text">Developed By Elijah Williams</p>
-        <p className="footer__year">2024</p>
-      </div>
-    </>
+    <footer className="footer">
+      <p className="footer__text">Developed By Elijah Williams</p>
+      <p className="footer__year">2024</p>
+    </footer>
   );
 }
 
